cf5db381ba410fda042341b39bb5481e85fac07d
khayan <khayanmalantrucco@gmail.com>
2020-05-10 20:40:36 -0300

    Cards gerados dinamicamente com JS e alinhados com Flex

--- js/api.js
+++ js/api.js
@@ -2,16 +2,18 @@ const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 async function buscarCribs() {
     try {
+        
         const response = await fetch(url);
-        const cribs = await response.json();
+        const cribs = await response.json();      
+
         cribs.map(crib => {
-            const { photo, property_type, name, price, address } = crib;
+            const { photo, property_type, name, price } = crib;
             criarCard(photo, property_type, name, price);
         });
+
     } catch (error) {
         console.log("Ocorreu um erro inesperado...");
     }
-    return cribs;
 }
 
 buscarCribs();
--- js/criar-card.js
+++ js/criar-card.js
@@ -1,10 +1,40 @@
+const ul = document.getElementById('lista-cribs');
+
 function criarCard(photo, property_type, name, price) {
+    const li = document.createElement('li');
+    li.classList.add('card');
 
+    const img = document.createElement('img');
+    img.classList.add('card-img');
+    img.src = photo;
+
+    const div = document.createElement('div');
+    div.classList.add('card-info');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('crib-title');
+    if(name.length > 30) {
+        h3.innerText = name
+            .substring(0, 30)
+            .concat('...')
+            .toLowerCase();
+    } else {
+        h3.innerText = name.toLowerCase();
+    }
+
+
+    const h4 = document.createElement('h4');
+    h4.classList.add('crib-type');
+    h4.innerText = property_type;
+
+    const p = document.createElement('p');
+    p.classList.add('crib-price');
+    p.innerText = `R$${price},00/dia`;
+
+    ul.appendChild(li);
+    li.appendChild(img);
+    li.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(p);
 }
-
-
-
-// photo
-// nome
-// property_type 
-// `R$${price},00/dia`
